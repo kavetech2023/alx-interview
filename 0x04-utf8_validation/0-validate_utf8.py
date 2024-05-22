64bbb18ba46951b3549bfb0fe98c5a7c39845e81
kavetech2023 <kavetech@gmail.com>
2024-05-22 09:58:52 +0300

    change variable name

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,9 +6,9 @@ def validUTF8(data):
     """method determines if given data set represents a valid UTF-8 encoding"""
     num_bytes = 0
     for num in data:
-        binary_rep = format(num, '#010b')[-8:]
+        binary_representation = format(num, '#010b')[-8:]
         if num_bytes == 0:
-            for bit in binary_rep:
+            for bit in binary_representation:
                 if bit == '0':
                     break
                 num_bytes += 1
@@ -17,7 +17,7 @@ def validUTF8(data):
             if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
+            if not (binary_representation[0] == '1' and binary_representation[1] == '0'):
                 return False
         num_bytes -= 1
     return num_bytes == 0
